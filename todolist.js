4fa6f3d1fb8f75d3152bd4d157f95eeb85f8ad09
Sang <thinsang1995@gmail.com>
2020-05-01 20:13:33 +0700

    Add JavaScript

---
+++ todolist.js
@@ -0,0 +1,52 @@
+//add items to Items List
+function addItem() {
+	const inputName = document.querySelector('#input-name').value;
+	let text = document.createTextNode(inputName);
+
+	let divWapper = document.createElement('div');
+	let itemName = document.createElement('h4');
+	divWapper.appendChild(itemName);
+	itemName.appendChild(text);
+
+	let question = document.createElement('label');
+	divWapper.appendChild(question);
+	question.appendChild(document.createTextNode("Did you do it? No, I din't"));
+
+	let checkedButton = document.createElement('input');
+	divWapper.appendChild(checkedButton);
+	checkedButton.setAttribute('type', 'checkbox');
+	checkedButton.setAttribute('onclick', 'checkedItem(this)');
+
+	let deleteButton = document.createElement('button');
+	divWapper.appendChild(deleteButton);
+	deleteButton.appendChild(document.createTextNode('Delete Me'));
+	deleteButton.setAttribute('onclick', 'deleteItem(this)');
+
+	//Data of status
+	if (inputName === '') {
+		return alert ('You must write something!');
+	} else {
+		let inputList = document.querySelector('#input-container');
+		inputList.appendChild(divWapper);
+	}
+	document.querySelector('#input-name').value = '';
+} 
+
+//Checked or not check
+function checkedItem(check) {
+	if (check.checked === true) {
+		check.previousElementSibling.textContent = "Did you do it? Yes, I did";
+	} else {
+		check.previousElementSibling.textContent = "Did you do it? No, I din't";
+	}
+} 
+
+//Delete items in Items List
+function deleteItem(deleteParent) {
+	deleteParent.parentNode.remove();
+}
+
+//Delete all items in Items List
+function deleteAll() {
+	document.querySelector('#input-container').innerHTML = '';
+}
